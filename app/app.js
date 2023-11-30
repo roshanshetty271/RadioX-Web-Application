dab16b0fc04a5edc3ed638ee62702359fcbb0ccf
Prasanna77-bit <144941047+Prasanna77-bit@users.noreply.github.com>
2023-11-30 17:37:12 -0500

    Merged all rest API

---
+++ app/app.js
@@ -0,0 +1,44 @@
+// ./app/app.js
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import registerRoutes from './routes/index.js';
+
+const initialize = (app) => {
+  // MongoDB URI
+  const mongoDBURI = 'mongodb+srv://shettyaayu:CAw8PmtbGCabGGZN@cluster0.7yywkny.mongodb.net/MasterDb?retryWrites=true&w=majority';
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // MongoDB connection
+  mongoose.connect(mongoDBURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Event listeners for Mongoose connection
+  const db = mongoose.connection;
+
+  // Successfully connected
+  db.on('connected', () => {
+    console.log(`Connected to MongoDB Atlas at ${mongoDBURI}`);
+  });
+
+  // Connection error
+  db.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+  });
+
+  // Disconnected
+  db.on('disconnected', () => {
+    console.log('MongoDB connection disconnected');
+  });
+  
+  // Register routes
+  registerRoutes(app);
+};
+
+// Export the initialize function
+export default initialize;
