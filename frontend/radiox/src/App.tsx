5ac4f8724f8f268103dcf2cf5599540b12a7f288
saurabhgangal <gangal.s@northeastern.edu>
2023-12-12 09:44:19 +0100

    update for app.tsx
    corrected the routing system using react routers

--- frontend/radiox/src/App.tsx
+++ frontend/radiox/src/App.tsx
@@ -1,25 +1,49 @@
-// App.js
+import React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { indigo, pink } from '@mui/material/colors';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home";
-import LoginScreen from "./components/Home/Login/Login-Screen"; // Update the path accordingly
-import ContactScreen from './components/Home/Contact/contact-us';
+import './App.css';
+import DwvComponent from './DwvComponent';
+import LandingPage from './Pages/LandingPage';
+import DoctorPortal from './Pages/DoctorPortal';
+import DwvPage from './Pages/DwvPage';
+import LoginPage from './Pages/LoginPage';
 
-function App() {
+export default function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = createTheme({
+    typography: {
+      useNextVariants: true,
+    },
+    palette: {
+      primary: {
+        main: indigo[500],
+      },
+      secondary: {
+        main: pink[500],
+      },
+      // mode: prefersDarkMode ? 'dark' : 'light',
+      mode: 'light'
+    },
+  });
+
   return (
-    <Router>
-      <div className="App">
-        {/* Your header or navigation bar can go here if needed */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <DwvComponent /> */}
+      <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/contact" element={<ContactScreen />} /> 
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/doctor" element={<DoctorPortal />} />
+          <Route path="/dwv" element={<DwvComponent />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dwv/:doctorId/:patientId" element={<DwvPage />} />
+
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
-
-export default App;
